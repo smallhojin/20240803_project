6ceb9630056171b076653653e0ef68a231203a28
smallhojin <uhwoong0830@gmail.com>
2024-10-12 11:30:52 +0900

    예제 1번 짝수 홀수 구별하기

--- python_1/IF 만약에.py
+++ python_1/IF 만약에.py
@@ -1,6 +1,7 @@
 # 사용자에게 수학 점수를 입력받아 상,중,하 반으로 나누기
 # 상 - 90점 이상
 
+""""
 size = int(input("수학 시험 점수를 입력해 주세요"))
 print(size)
 
@@ -13,7 +14,15 @@ if 0 <= size <= 100 :
         print("중반으로 가십시오")
 else:
     print("점수를 올바르게 입력해 주세요.")
+"""
 
+size = int(input("자연수를 입력해 주세요"))
+print(size)
+
+if int(size % 2) == 0 :
+    print("짝수 입니다")
+elif int(size % 2) == 1 :
+    print("홀수 입니다")
 
 
 
