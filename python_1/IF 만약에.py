5b672e99f75eae495824ac5b56742d41faa563d9
smallhojin <uhwoong0830@gmail.com>
2024-10-12 11:50:15 +0900

    예제 3번 초반까지

--- python_1/IF 만약에.py
+++ python_1/IF 만약에.py
@@ -25,14 +25,16 @@ else:
 #elif int(size % 2) == 1 :
    # print("홀수 입니다")
 
-con = str(input("아무 문장이나 입력해 주세요"))
-print(con)
-main_con = con
-key_con = "파이썬"
+#con = str(input("아무 문장이나 입력해 주세요"))
+#print(con)
+#main_con = con
+#key_con = "파이썬"
 
-if key_con in main_con :
-    print("파이썬과 관련된 이야기 이군요!")
-else:
-    print("오 그렇군요!!")
+#if key_con in main_con :
+    #print("파이썬과 관련된 이야기 이군요!")
+#else:
+    #print("오 그렇군요!!")
 
+per = str(input("당신은 주말에 보통 무엇을 하며 시간을 보내나요? "))
+print(per)
 
