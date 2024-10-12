bbf158edbfbe992bd97d950edb38069945f8766e
smallhojin <uhwoong0830@gmail.com>
2024-10-12 10:16:46 +0900

    boolean(True/False) 예제

---
+++ IF 문.py
@@ -0,0 +1,31 @@
+# True / False : 첫 글자는 대문자
+# 비교 연산자는 등호 2개
+a = 10
+b = 10
+c = 15
+
+
+print(True)
+print("1==true", 1==False)
+print("0==true", 0==False)
+print("2==true", 2==False)
+
+print("1==true", 1==True)
+print("0==true", 0==True)
+print("2==true", 2==True)
+
+print("a==b", a==b)
+print("a==b", a!=b)
+print("a==b", a>b)
+print("a==b", a<b)
+print("a==b", a>=b)
+print("a==b", a<=b)
+
+print("===================")
+
+print("a==c", a==c)
+print("a==c", a!=c)
+print("a==c", a>c)
+print("a==c", a<c)
+print("a==c", a>=c)
+print("a==c", a<=c)
