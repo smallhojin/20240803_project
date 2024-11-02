20885a3c128329de05736d3151800e9cfd554e73
smallhojin <uhwoong0830@gmail.com>
2024-11-02 10:52:45 +0900

    숫자 맞추기 프로그램

--- python_1/반복문.py
+++ python_1/반복문.py
@@ -1,6 +1,6 @@
 
 
-num = -1
+"""num = -1
 
 while num > 100 or num < 0:
     num = int(input("수학 점수는?"))
@@ -13,7 +13,18 @@ elif num > 60:
     print("B반")
 else:
     print("C반")
+    """
+import random
 
+answer = int(random.randrange(1,100))
+um = int(input("어떤 숫자인지 1에서 100까지의 수중 하나를 선택하세요"))
+print(answer)
+while answer != um :
+    answer = int(random.randrange(1, 100))
+    um = int(input("어떤 숫자인지 1에서 100까지의 수중 하나를 선택하세요"))
+    print(answer)
+if answer == um :
+     print("good")
 
 
 
